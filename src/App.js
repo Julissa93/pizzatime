eea646c773cc5c6e7cf58ab352da7e45e0f0cd18
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2019-08-13 13:57:27 -0400

    added src files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const Topping = props => {
+  const isSelected = props.selectedTopping === props.name;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <li className={isSelected ? 'selected' : ''} onClick={() => props.handleClick(props.name)}>{props.name}</li>
+  )
 }
 
-export default App;
+class ToppingList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedTopping: 'Cheese',
+      toppings: ['Cheese', 'Anchovies', 'Broccoli']
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick (name) {
+    //console.log('clicked !!!!! ' , name);
+    this.setState({
+      selectedTopping: name
+    })
+  }
+
+  render() {
+    console.log('IN RENDER () - this.state.toppings = ', this.state.toppings)
+    return (
+      <div>
+        <h1> Pizza Time!!!! </h1>
+        <h2>Your fave pizza topping is: {this.state.selectedTopping}</h2>
+        <ul >
+          {
+            /* Instead of rendering the Topping component three seperate times we can just map through our toppings array! */
+            this.state.toppings.map((topping, index )=>( 
+            <Topping key={index} name={topping} selectedTopping={this.state.selectedTopping} handleClick={this.handleClick}/>)
+          )
+          }
+
+          {/*<Topping name="Mushrooms" selectedTopping={this.state.selectedTopping} handleClick={this.handleClick}/>
+          <Topping name="Olives" selectedTopping={this.state.selectedTopping} handleClick={this.handleClick}/>
+          <Topping name="Cheese" selectedTopping={this.state.selectedTopping} handleClick={this.handleClick}/>*/}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default ToppingList;
